6a17def5e232395b1d8e6658815de61f44f8679c
hasindi <hmudithya@gmail.com>
2022-05-19 12:56:47 +0530

    refactored query

---
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -0,0 +1,35 @@
+package dao;
+
+import db.DBConnection;
+import model.CustomerDTO;
+import model.ItemDTO;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ItemDAOImpl {
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+        ArrayList<ItemDTO> allItems = new ArrayList<>();
+        while (rst.next()) {
+            String code = rst.getString(1);
+            String description = rst.getString(2);
+            BigDecimal unitPrice = rst.getBigDecimal(3);
+            int qtyOnHand = rst.getInt(4);
+            getAllItems().add(new ItemDTO(code, description, unitPrice, qtyOnHand));
+        }
+        return allItems;
+    }
+
+    public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Item WHERE code=?");
+        pstm.setString(1, code);
+        return pstm.executeUpdate() > 0;
+    }
+
+
+}
