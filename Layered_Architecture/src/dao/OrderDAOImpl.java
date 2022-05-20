24b3e93d4ef6f7b6f53a1db08d1fae6410a59262
hasindi <hmudithya@gmail.com>
2022-05-20 14:27:59 +0530

    refactered PlaceOrderForm with OrderDAO layer

--- Layered_Architecture/src/dao/OrderDAOImpl.java
+++ Layered_Architecture/src/dao/OrderDAOImpl.java
@@ -1,9 +1,10 @@
 package dao;
 
+import db.DBConnection;
 import model.CustomerDTO;
 import model.OrderDTO;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class OrderDAOImpl implements CrudDAO<OrderDTO,String> {
@@ -15,7 +16,7 @@ public class OrderDAOImpl implements CrudDAO<OrderDTO,String> {
 
     @Override
     public boolean save(OrderDTO dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return SQLUtil.executeUpdate("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)",dto.getOrderId(),dto.getOrderDate(),dto.getCustomerId());
     }
 
     @Override
@@ -29,8 +30,8 @@ public class OrderDAOImpl implements CrudDAO<OrderDTO,String> {
     }
 
     @Override
-    public boolean exist(String s) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean exist(String oId) throws SQLException, ClassNotFoundException {
+        return SQLUtil.executeQuery("SELECT oid FROM `Orders` WHERE oid=?",oId).next();
     }
 
     @Override
@@ -40,6 +41,8 @@ public class OrderDAOImpl implements CrudDAO<OrderDTO,String> {
 
     @Override
     public String generateNewId() throws SQLException, ClassNotFoundException {
-        return null;
+        ResultSet rst = SQLUtil.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
+        return rst.next() ? String.format("OID-%03d",
+                (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
     }
 }
--- Layered_Architecture/src/dao/OrderDetailDAOImpl.java
+++ Layered_Architecture/src/dao/OrderDetailDAOImpl.java
@@ -16,7 +16,11 @@ public class OrderDetailDAOImpl implements CrudDAO<OrderDetailDTO,String> {
 
     @Override
     public boolean save(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
-        return false;
+        stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
+        /*stm.setString(1, orderId);
+                stm.setString(2, detail.getItemCode());
+                stm.setBigDecimal(3, detail.getUnitPrice());
+                stm.setInt(4, detail.getQty());*/
     }
 
     @Override
