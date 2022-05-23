146724a2c8aa2716e3f752dc2c29521f4c29510e
hasindi <hmudithya@gmail.com>
2022-05-23 22:38:45 +0530

    Create impl package in custom package and add impl layers

--- Layered_Architecture/src/dao/custom/impl/OrderDetailDAOImpl.java
+++ Layered_Architecture/src/dao/custom/impl/OrderDetailDAOImpl.java
@@ -1,8 +1,46 @@
-package dao;
+package dao.custom.impl;
 
 import dao.custom.OrderDetailDAO;
+import model.OrderDetailDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class OrderDetailDAOImpl implements OrderDetailDAO {
+    @Override
+    public ArrayList<OrderDetailDTO> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public OrderDetailDTO search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+        return null;
+    }
 
 
    /* @Override
